505d72fa29d33419ecd12e6385f1922e9cf22d15
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2023-08-20 21:57:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,17 +1,17 @@
 
 <?php
 echo "Hi";
-/*
-$hostname = "localhost";
-$username = 'root';
-$password = '';
-$dbname = 'ers_db';
+
+$hostname = "db4free.net";
+$username = 'ers_fos_admin';
+$password = 'd@*a!r0NEfzv7ZwQ';
+$dbname = 'ers_fos_db';
 
 $con = mysqli_connect($hostname, $username, $password, $dbname);
 
 if (!$con) {
     echo "db not";
     //die("Connection failed : " . mysqli_connect_error());
-}*/
+}
 
 ?>
