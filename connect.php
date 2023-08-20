439e70e8914127136e4cb1391b9489f291bb0ca4
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2023-08-20 21:44:16 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,7 +8,8 @@ $dbname = 'ers_db';
 $con = mysqli_connect($hostname, $username, $password, $dbname);
 
 if (!$con) {
-    die("Connection failed : " . mysqli_connect_error());
+    echo "db not";
+    //die("Connection failed : " . mysqli_connect_error());
 }
 
 ?>
