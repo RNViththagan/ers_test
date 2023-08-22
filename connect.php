5212b2dafa95626b3148d6417c24e37e749716bb
Viththagan R.N <84705898+RNViththagan@users.noreply.github.com>
2023-08-22 19:17:38 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,17 +1,16 @@
-
 <?php
-echo "Hi";
-
-$hostname = "db4free.net";
-$username = 'ers_fos_admin';
-$password = 'd@*a!r0NEfzv7ZwQ';
-$dbname = 'ers_fos_db';
-
-$con = mysqli_connect($hostname, $username, $password, $dbname);
-
-if (!$con) {
-    echo "db not";
-    //die("Connection failed : " . mysqli_connect_error());
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:ers-fos.database.windows.net,1433; Database = ers-fos-db", "CloudSA5c33d5c2", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSA5c33d5c2", "pwd" => "D38627I60J4BDCZ4$", "Database" => "ers-fos-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:ers-fos.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
